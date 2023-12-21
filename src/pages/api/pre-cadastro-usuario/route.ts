b8e8b021c0f8ac988546c49ccd94b4e8bdf5f460
ronycode <ronypc@outlook.com>
2023-12-21 12:55:14 -0300

    tentando deploy

---
+++ src/pages/api/pre-cadastro-usuario/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const { email } = await request.json()
+
+  if (!email)
+    return NextResponse.json({ message: 'Erro parâmetros necessários' })
+
+  const res = await fetch(`${process.env.API_GSO}/api/auth/pre-cadastro`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email }),
+  })
+
+  if (!res.ok) {
+    return NextResponse.json({ ...(await res?.json()) }, { status: 400 })
+  }
+  return NextResponse.json(
+    {
+      data: true,
+      status: 'success',
+      code: 200,
+      message: 'Email enviado com sucesso!',
+    },
+    { status: 200 },
+  )
+}
