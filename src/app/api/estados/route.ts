992ea6a35c4e1310ee469f18d2d7ab34c1533a02
ronycode <ronypc@outlook.com>
2023-12-23 15:12:20 -0300

    tentando deploy

--- src/app/api/estados/route.ts
+++ src/app/api/estados/route.ts
@@ -1,20 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-  if (!res.ok) {
-    return NextResponse.json(
-      { message: res.statusText },
-      { status: res.status },
-    )
-  }
-  const data = await res.json()
-
-  return NextResponse.json(data)
-  // return NextResponse.json([{ data: 'data' }])
+  // const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // })
+  // if (!res.ok) {
+  //   return NextResponse.json(
+  //     { message: res.statusText },
+  //     { status: res.status },
+  //   )
+  // }
+  // const data = await res.json()
+  //
+  // return NextResponse.json(data)
+  return NextResponse.json([{ data: 'data' }])
 }
