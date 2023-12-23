1efd67b0905cd5b766e17ddbcb7454ae7c941c6d
ronycode <ronypc@outlook.com>
2023-12-23 16:01:06 -0300

    tentando deploy

--- src/app/api/estados/route.ts
+++ src/app/api/estados/route.ts
@@ -2,19 +2,23 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   // const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // })
-  // if (!res.ok) {
-  //   return NextResponse.json(
-  //     { message: res.statusText },
-  //     { status: res.status },
-  //   )
-  // }
-  // const data = await res.json()
-  //
-  // return NextResponse.json(data)
-  return NextResponse.json([{ data: 'data' }])
+  const res = await fetch(
+    'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+  if (!res.ok) {
+    return NextResponse.json(
+      { message: res.statusText },
+      { status: res.status },
+    )
+  }
+  const data = await res.json()
+
+  return NextResponse.json(data)
+  // return NextResponse.json([{ data: 'data' }])
 }
