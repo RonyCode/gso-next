4e14d13c26fce38c84f8422b74818f41ba09d64a
ronycode <ronypc@outlook.com>
2023-12-23 15:02:15 -0300

    tentando deploy

--- src/app/api/estados/route.ts
+++ src/app/api/estados/route.ts
@@ -1,20 +1,20 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  // const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // })
-  // if (!res.ok) {
-  //   return NextResponse.json(
-  //     { message: res.statusText },
-  //     { status: res.status },
-  //   )
-  // }
-  // const data = await res.json()
-  //
-  // return NextResponse.json(data)
-  return NextResponse.json([{ data: 'data' }])
+  const res = await fetch('http://wsgso.localhost/api/auth/estados', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  if (!res.ok) {
+    return NextResponse.json(
+      { message: res.statusText },
+      { status: res.status },
+    )
+  }
+  const data = await res.json()
+
+  return NextResponse.json(data)
+  // return NextResponse.json([{ data: 'data' }])
 }
