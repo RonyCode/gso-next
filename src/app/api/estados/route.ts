28b5adf5b03cd80ff81577712f973320a335e49d
ronycode <ronypc@outlook.com>
2023-12-26 13:19:15 -0300

    tentando deploy

--- src/app/api/estados/route.ts
+++ src/app/api/estados/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
   // const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
-  const res = await fetch(`${process.env.API_GSO}/api/auth/estados`, {
+  const res = await fetch('https://wsgso.000webhostapp.com/api/auth/estados', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
