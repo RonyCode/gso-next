b9644c6172847748fa01614e8c44013ef44f0ba2
ronycode <ronypc@outlook.com>
2023-12-28 09:06:20 -0300

    tentando deploy

--- src/lib/getAllStates.tsx
+++ src/lib/getAllStates.tsx
@@ -3,15 +3,12 @@ import { AddressProps } from '../../types'
 import { stateStore } from '@/stores/Address/stateStore'
 
 export const getAllStates = async () => {
-  const res = await fetchWrapper<AddressProps[]>(
-    'http://localhost:3000/api/estados',
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  const res = await fetchWrapper<AddressProps[]>('/api/estados', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  )
+  })
   stateStore.setState({ states: res })
   return res
 }
