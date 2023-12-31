73cdbafe653b107ed8571bb4412ab15511687515
ronycode <ronypc@outlook.com>
2023-12-31 16:20:49 -0300

    tentando deploy

--- src/app/(auth)/cadastro-usuario/[token]/page.tsx
+++ src/app/(auth)/cadastro-usuario/[token]/page.tsx
@@ -1,15 +1,23 @@
-import { UserRegisterForm } from '@/app/(auth)/cadastro-usuario/[token]/components/UserRegisterForm'
 import MaxWidthWrapper from '@/components/Layout/MaxWidthWrapper'
 import { TokenVerify } from '@/functions/TokenVerify'
 import { CardWithLogo } from '@/ui/CardWithLogo'
 import { useUserStore } from '@/stores/user/userStore'
 import { UserType } from '../../../../../types/index'
+import { Metadata } from 'next'
+import { UserRegisterForm } from '@/app/(auth)/cadastro-usuario/[token]/components/UserRegisterForm'
+
+export const metadata: Metadata = {
+  title: 'GSO | Cadastro',
+  description: 'page of signUp users.',
+}
 
 const CadastroUsuario = async ({ params }: { params: { token: string } }) => {
   const tokenReplaced = params.token.replaceAll('%2B', '.')
 
   const jwtValid = await TokenVerify(tokenReplaced)
-
+  useUserStore.setState({
+    state: { user: { email: tokenReplaced as string } as UserType },
+  })
   return (
     <>
       <MaxWidthWrapper className="mt-24 px-6 lg:mt-0 lg:w-7/12 lg:px-0 ">
