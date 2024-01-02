8b76653bf112fe1d08ca257cd1e292e05eecf0e3
ronycode <ronypc@outlook.com>
2024-01-02 20:53:56 -0300

    tentando deploy

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,14 +6,15 @@ export async function GET(request: Request) {
   deleteCookies()
 
   if (typeof window !== 'undefined') {
+    deleteCookies()
+
     await signOut({
-      callbackUrl: process.env.NEXT_PUBLIC_NEXT_URL,
       redirect: false,
     })
   }
 
-  return NextResponse.next()
-  // return NextResponse.redirect(
-  //   new URL(process.env.NEXT_PUBLIC_NEXT_URL + '/auth', request.url),
-  // )
+  // return NextResponse.next()
+  return NextResponse.redirect(
+    new URL(process.env.NEXT_PUBLIC_NEXT_URL!, request.url),
+  )
 }
