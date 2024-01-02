c4320ec7dbd4b077399c4082e449b58b24b46f07
ronycode <ronypc@outlook.com>
2024-01-02 15:11:57 -0300

    tentando deploy

--- src/app/(auth)/cadastro-usuario/[token]/page.tsx
+++ src/app/(auth)/cadastro-usuario/[token]/page.tsx
@@ -13,36 +13,18 @@ export const metadata: Metadata = {
 }
 
 const CadastroUsuario = async ({ params }: { params: { token: string } }) => {
-  const tokenReplaced = params.token.replaceAll('%2B', '.')
+  const tokenReplaced = params.token
+    .replaceAll('%2B', '.')
+    .replaceAll('%2B', '.')
 
-  // const jwtValid = await TokenVerify(tokenReplaced)
+  const jwtValid = await TokenVerify(tokenReplaced)
 
-  const dateNowConverted = new Date().toLocaleString('pt-BR', {
-    timeZone: 'America/Araguaina',
-  })
-
-  // const payload = decodeJwt(tokenReplaced)
-  // const dateExpires = payload!.exp!
-
-  const dateExpiresCpnverted = new Date(1704229070 * 1000).toLocaleString(
-    'pt-BR',
-    {
-      timeZone: 'America/Araguaina',
-    },
-  )
   return (
     <>
-      <div className="p-24 text-center">
-        {tokenReplaced}
-        <br></br>
-        {dateNowConverted}
-        <br></br>
-
-        {dateExpiresCpnverted}
-      </div>
+      <div className="p-24 text-center"></div>
       <MaxWidthWrapper className="mt-24 px-6 lg:mt-0 lg:w-7/12 lg:px-0 ">
-        {dateNowConverted > dateExpiresCpnverted ? (
-          <UserRegisterForm params={'jwtValid.email' || ''} />
+        {jwtValid ? (
+          <UserRegisterForm params={jwtValid.email || ''} />
         ) : (
           <CardWithLogo>
             Token inválido ou expirado por favor tente novamente
