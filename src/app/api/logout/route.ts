927ba650e6f4cecce66580a5688942fe4a1556d9
ronycode <ronypc@outlook.com>
2024-01-02 21:49:52 -0300

    tentando deploy

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,44 +6,34 @@ export async function GET(request: Request) {
     { status: 200 },
   )
 
-  userLogoutResponse.cookies.set({
-    name: 'next-auth.session-token',
-    value: '',
+  userLogoutResponse.cookies.set('next-auth.session-token', '', {
     httpOnly: true,
-    maxAge: 0,
-    path: '/',
+    expires: new Date(0),
   })
 
-  userLogoutResponse.cookies.set({
-    name: '__Secure-next-auth.session-token',
-    value: '',
+  userLogoutResponse.cookies.set('__Secure-next-auth.session-token', '', {
     httpOnly: true,
-    maxAge: 0,
-    path: '/',
+    expires: new Date(0),
   })
 
-  userLogoutResponse.cookies.set({
-    name: 'next-auth.csrf-token',
-    value: '',
+  userLogoutResponse.cookies.set('next-auth.csrf-token', '', {
     httpOnly: true,
-    maxAge: 0,
-    path: '/',
+    expires: new Date(0),
   })
 
-  userLogoutResponse.cookies.set({
-    name: '__Host-next-auth.csrf-token',
-    value: '',
+  userLogoutResponse.cookies.set('__Host-next-auth.csrf-token', '', {
     httpOnly: true,
-    maxAge: 0,
-    path: '/',
+    expires: new Date(0),
   })
 
-  userLogoutResponse.cookies.set({
-    name: 'next-auth.callback-url',
-    value: '',
+  userLogoutResponse.cookies.set('next-auth.callback-url', '', {
     httpOnly: true,
-    maxAge: 0,
-    path: '/',
+    expires: new Date(0),
+  })
+
+  userLogoutResponse.cookies.set('next-auth.callback-url', '', {
+    httpOnly: true,
+    expires: new Date(0),
   })
 
   userLogoutResponse.cookies.set({
