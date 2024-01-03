6a8be9dce38ce8cd7586664ddf1f237d119ffc2d
ronycode <ronypc@outlook.com>
2024-01-02 21:59:25 -0300

    tentando deploy

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -7,42 +7,52 @@ export async function GET(request: Request) {
     { status: 200 },
   )
 
-  userLogoutResponse.cookies.set('next-auth.session-token', '', {
-    httpOnly: true,
-    expires: new Date(0),
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.session-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
   })
 
-  userLogoutResponse.cookies.set('__Secure-next-auth.session-token', '', {
-    httpOnly: true,
-    expires: new Date(0),
+  userLogoutResponse.cookies.set({
+    name: '__Secure-next-auth.session-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
   })
 
-  userLogoutResponse.cookies.set('next-auth.csrf-token', '', {
-    httpOnly: true,
-    expires: new Date(0),
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.csrf-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
   })
 
-  userLogoutResponse.cookies.set('__Host-next-auth.csrf-token', '', {
-    httpOnly: true,
-    expires: new Date(0),
+  userLogoutResponse.cookies.set({
+    name: '__Host-next-auth.csrf-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
   })
 
-  userLogoutResponse.cookies.set('next-auth.callback-url', '', {
-    httpOnly: true,
-    expires: new Date(0),
-  })
-
-  userLogoutResponse.cookies.set('next-auth.callback-url', '', {
-    httpOnly: true,
-    expires: new Date(0),
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.callback-url',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
   })
 
   userLogoutResponse.cookies.set({
     name: '__Secure-next-auth.callback-url',
     value: '',
-    httpOnly: true,
-    maxAge: 0,
+    expires: new Date('2016-10-05'),
     path: '/',
+    maxAge: 0,
   })
 
   return redirect('/')
