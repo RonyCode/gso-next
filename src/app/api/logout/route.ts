f16bd17d992675b28cd2196c731b1294e853b830
ronycode <ronypc@outlook.com>
2024-01-02 21:30:51 -0300

    tentando deploy

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,20 +1,63 @@
 import { NextResponse } from 'next/server'
 import { deleteCookies } from '@/components/Buttoms/SignOutButton/LogoutAction'
 import { signOut } from 'next-auth/react'
+import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
   deleteCookies()
 
-  if (typeof window !== 'undefined') {
-    deleteCookies()
-
-    await signOut({
-      redirect: false,
-    })
-  }
-
-  // return NextResponse.next()
-  return NextResponse.redirect(
-    new URL(process.env.NEXT_PUBLIC_NEXT_URL!, request.url),
+  const userLogoutResponse = NextResponse.json(
+    { message: 'User is logged out', sucess: true },
+    { status: 200 },
   )
+
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.session-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  userLogoutResponse.cookies.set({
+    name: '__Secure-next-auth.session-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.csrf-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  userLogoutResponse.cookies.set({
+    name: '__Host-next-auth.csrf-token',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  userLogoutResponse.cookies.set({
+    name: 'next-auth.callback-url',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  userLogoutResponse.cookies.set({
+    name: '__Secure-next-auth.callback-url',
+    value: '',
+    expires: new Date('2016-10-05'),
+    path: '/',
+    maxAge: 0,
+  })
+
+  return userLogoutResponse
 }
