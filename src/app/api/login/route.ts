49ecdd605c0df91f58b508dba307a7ad47db73a6
ronycode <ronypc@outlook.com>
2024-03-04 23:23:50 -0300

    implementando o lado conta

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,26 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { SignInSchema } from '@/app/(auth)/auth/schemas/SignInSchema'
-import { limiter } from '@/app/api/config/limiter'
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const origin: string | null = request.headers.get('origin')
-  const remaining: number = await limiter.removeTokens(1)
   const body: SignInSchema = await request.json()
   const { email, senha, is_user_external: isUserExternal } = body
 
   if (!email || !senha)
     return NextResponse.json({ message: 'Erro parametros necessários' })
-
-  if (remaining < 0) {
-    return new NextResponse(null, {
-      status: 429,
-      statusText: 'To many requests',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    })
-  }
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_GSO}/api/auth/login`, {
     method: 'POST',
@@ -31,7 +19,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     body: JSON.stringify({
       email,
       senha,
-      isUserExternal,
+      is_user_external: isUserExternal,
     }),
   })
 
