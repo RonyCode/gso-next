5e83890e73ca26d6fe5994c568774720b3abb869
ronycode <ronypc@outlook.com>
2024-04-08 19:37:21 -0300

    servidor

--- src/lib/getAllStates.ts
+++ src/lib/getAllStates.ts
@@ -4,7 +4,7 @@ import { stateStore } from '@/stores/Address/stateStore'
 
 export const getAllStates = async () => {
   const res = await fetchWrapper<AddressProps[]>(
-    `${process.env.NEXT_PUBLIC_NEXT_URL}/api/estados`,
+    'https://gso-dev.vercel.app//api/estados',
     {
       method: 'GET',
       headers: {
