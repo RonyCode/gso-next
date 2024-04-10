4372647afb1aa80dd411e0e0ae57b7b14e679136
ronycode <ronypc@outlook.com>
2024-04-10 20:49:45 -0300

    servidor

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -11,11 +11,8 @@ export const runtime = 'edge'
 export default function Page() {
   return (
     <div className="w-screen lg:container">
-      {process.env.NODE_ENV} {''}
       <br />
-      {process.env.NEXT_PUBLIC_API_GSO}
       <br />
-      {process.env.NEXT_PUBLIC_NEXT_URL}
       <LoginFormSlice />
     </div>
   )
