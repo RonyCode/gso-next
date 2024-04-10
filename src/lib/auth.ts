a7db6370a33de15400cb478c6d9f639e31107fc4
ronycode <ronypc@outlook.com>
2024-04-10 09:18:43 -0300

    servidor

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,12 +22,11 @@ export const confereLogado = async (payload: {
   senha?: string
   is_user_external?: number
 }) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_NEXT}/api/login`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_NEXT_URL}/api/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload),
   })
-
   if (res.ok) {
     return await res.json()
   } else {
