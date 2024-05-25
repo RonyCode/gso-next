1ad179912e2262242bdc9940afc7a64b5003b182
ronycode <ronypc@outlook.com>
2024-05-24 23:59:59 -0300

    refatorando com eslint

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   experimental: {
     turbo: {
       rules: {
-        "*.svg": {
-          loaders: ["@ducanh2912/next-pwa"],
-          as: "*.js",
+        '*.svg': {
+          loaders: ['@ducanh2912/next-pwa'],
+          as: '*.js',
         },
       },
     },
@@ -16,44 +16,43 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "www.designi.com.br",
+        protocol: 'https',
+        hostname: 'www.designi.com.br',
       },
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "dpobjetivo.com.br",
+        protocol: 'https',
+        hostname: 'dpobjetivo.com.br',
       },
       {
-        protocol: "https",
-        hostname: "apexpublicschool.com",
+        protocol: 'https',
+        hostname: 'apexpublicschool.com',
       },
       {
-        protocol: "http",
-        hostname: "wsgso.000webhostapp.com",
+        protocol: 'http',
+        hostname: 'wsgso.000webhostapp.com',
       },
       {
-        protocol: "http",
-        hostname: "wsgso.localhost",
+        protocol: 'http',
+        hostname: 'wsgso.localhost',
       },
       {
-        protocol: "http",
-        hostname: "192.168.100.50",
+        protocol: 'http',
+        hostname: '192.168.100.50',
       },
     ],
   },
-};
+}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-undef
 module.exports = (phase) => {
   // if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    customWorkerSrc: "service-worker",
+  const withPWA = require('@ducanh2912/next-pwa').default({
+    dest: 'public',
+    customWorkerSrc: 'service-worker',
     disable: false,
     // disable: process.env.NODE_ENV === 'development',
     cacheOnFrontEndNavigation: true,
@@ -64,8 +63,8 @@ module.exports = (phase) => {
     workboxOptions: {
       disableDevLogs: true,
     },
-  });
-  return withPWA(nextConfig);
+  })
+  return withPWA(nextConfig)
   // }
   // return nextConfig
-};
+}
