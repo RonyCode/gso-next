26b972843b7f14b5e0315cc07fa2115ea17ebb94
ronycode <ronypc@outlook.com>
2024-05-25 00:16:22 -0300

    refatorando com eslint

--- src/app/api/cep/route.ts
+++ src/app/api/cep/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
+export async function GET(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url)
   const cep = searchParams.get('cep')
   const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
--- src/app/api/estados/route.ts
+++ src/app/api/estados/route.ts
@@ -1,21 +1,21 @@
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
-export async function GET(): Promise<NextResponse> {
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_GSO}/services/states`,
-  //   {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   },
-  // )
-  // if (!res.ok) {
-  //   return NextResponse.json(
-  //     { message: res.statusText },
-  //     { status: res.status },
-  //   )
-  // }
-  // const data = await res.json()
-  return NextResponse.json('data')
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_GSO}/services/states`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+  if (!res.ok) {
+    return NextResponse.json(
+      { message: res.statusText },
+      { status: res.status },
+    )
+  }
+  const data = await res.json()
+  return NextResponse.json(data)
 }
