3b9b59c686582ae31cd51fdfb1f74467b7623edd
ronycode <ronypc@outlook.com>
2024-06-29 12:19:26 -0300

    fazendo lista escala

--- src/components/Cards/CardListEscala.tsx
+++ src/components/Cards/CardListEscala.tsx
@@ -1,20 +1,15 @@
-import { getServerSession } from 'next-auth'
 import React, { type ReactElement } from 'react'
-import { useForm } from 'react-hook-form'
 import { BsBuildingCheck } from 'react-icons/bs'
 import { GrGroup } from 'react-icons/gr'
-import { LuCalendarDays, LuClock, LuUser } from 'react-icons/lu'
+import { LuCalendarDays, LuClock } from 'react-icons/lu'
 import { MdOutlineMapsHomeWork } from 'react-icons/md'
 import { RiPoliceCarLine } from 'react-icons/ri'
-import { toast } from 'react-toastify'
 
 import { cn } from '@/lib/utils'
 import { type IScheduleSchema } from '@/schemas/ScheduleSchema'
 import { type IUnidadeSchema } from '@/schemas/UnidadeSchema'
 import { Badge } from '@/ui/badge'
-import { Button } from '@/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/ui/card'
-import { Form, FormControl, FormField, FormItem, FormLabel } from '@/ui/form'
 import { Label } from '@/ui/label'
 import {
   SelectItem,
@@ -24,11 +19,10 @@ import {
   Select,
 } from '@/ui/select'
 import { Separator } from '@/ui/separator'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
-import { z } from 'zod'
+import { scheduler } from 'node:timers/promises'
 
 type CardProps = {
   itemEvent: IScheduleSchema
@@ -43,13 +37,19 @@ export const CardListEscala = ({
   className,
   ...props
 }: CardProps): ReactElement => {
+  unidade?.companySchedules?.map((schedule, index) => {
+    if (schedule?.schedule?.id === itemEvent?.id) {
+      console.log(schedule?.schedule.cars)
+    }
+  })
+
   return (
     <>
       <Card className={cn(className)} {...props}>
         <CardHeader className="justify-center p-2 md:p-3">
           <div className="m-0 flex justify-evenly p-0 md:justify-between">
             <CardTitle className="flex w-full items-center justify-between gap-1">
-              <div className="text-md font-bold ">
+              <div className="text-md font-bold">
                 <Badge
                   className={` block ${
                     itemEvent.team === 1
@@ -108,178 +108,26 @@ export const CardListEscala = ({
         </CardHeader>
         <Separator />
         <CardContent>
-          <div className=" grid grid-cols-1  md:grid-cols-12 ">
-            <div className="col-start-1 col-end-4 ">
-              <div className="item-center flex  flex-col justify-center  pb-3 text-sm font-medium">
-                <div className=" flex items-center gap-2 rounded-[8px] border  border-primary/60  p-1 md:border-0 md:border-b  ">
-                  <div>
-                    <Avatar
-                      className="
-                      flex h-14 w-14  items-center justify-center rounded-full
-                      shadow-sm shadow-foreground transition-all duration-300 hover:scale-[200%] "
-                    >
-                      <AvatarImage
-                        className="aspect-square rounded-full object-cover"
-                        src={unidade?.image}
-                      />
-                      <AvatarFallback>
-                        {<BsBuildingCheck size={36} />}
-                      </AvatarFallback>
-                    </Avatar>
-                  </div>
-                  <div>
-                    <p className="text-sm font-medium leading-none">Unidade </p>
-                  </div>
-                  <Select defaultValue="edit">
-                    <SelectTrigger className="ml-auto w-[110px]">
-                      <SelectValue placeholder="Selecione" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="edit">{unidade?.name}</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
+          {unidade?.companySchedules?.map((schedule, index) => (
+            <div key={index}>
+              {schedule?.schedule?.id === itemEvent?.id && (
+                <div>
+                  <div>Comunicação</div>
 
-              <div className="grid  w-full grid-cols-2 gap-4  p-3">
-                <div className="item-start flex flex-col justify-center  text-sm font-medium">
-                  <div className="flex  items-center gap-1 ">
-                    <i>
-                      <MdOutlineMapsHomeWork />
-                    </i>
-                    <Label> id unidade</Label>
-                  </div>
-                  <p className="text-sm font-medium">{unidade?.id}</p>
-                </div>
-                <div className="item-start flex flex-col justify-center  text-sm font-medium">
-                  <div className="flex  items-center gap-1 ">
-                    <i>
-                      <MdOutlineMapsHomeWork />
-                    </i>
-                    <Label> Tipo</Label>
-                  </div>
-                  <p className="text-sm font-medium">{unidade?.type}</p>
-                </div>
-                <div className="item-start flex flex-col justify-center  text-sm font-medium">
-                  <div className="flex  items-center gap-1 ">
-                    <i>
-                      <MdOutlineMapsHomeWork />
-                    </i>
-                    <Label> id schedule</Label>
-                  </div>
-                  <p className="text-sm font-medium">{itemEvent?.id}</p>
-                </div>
-                <div className="item-start flex flex-col justify-center  text-sm font-medium">
-                  <div className="flex  items-center  gap-1">
-                    <i>
-                      <MdOutlineMapsHomeWork />
-                    </i>
-                    <Label> Companhia</Label>
-                  </div>
-                  <p className="text-sm font-medium">{unidade?.name}</p>
-                </div>
-                <div className="item-start flex flex-col justify-center  text-sm font-medium">
-                  <div className="flex  items-center  gap-1">
-                    <i>
-                      <MdOutlineMapsHomeWork />
-                    </i>
-                    <Label> Cidade</Label>
-                  </div>
-                  <p className="text-sm font-medium">
-                    {unidade?.companyAddress?.city}
-                  </p>
-                </div>
-              </div>
-            </div>
-            {unidade?.companySchedules?.map((itemSchedule, index) => (
-              <div key={index} className="col-start-4 col-end-13">
-                {itemSchedule?.schedule.id === itemEvent?.id && (
-                  <div className="col-span-3">
-                    {itemSchedule?.cars?.map((carSchedule, inderxCa) => (
-                      <div
-                        key={inderxCa}
-                        className=" col-start-1 col-end-9  flex w-full items-center justify-between gap-2 rounded-[8px] border  border-primary/60  p-1 md:border-0 md:border-b "
-                      >
-                        <Avatar
-                          className="
-                                flex h-14 w-14  items-center justify-center rounded-full
-                                shadow-sm shadow-foreground transition-all duration-300 hover:scale-[200%] "
-                        >
-                          <AvatarImage
-                            className="aspect-square rounded-full object-cover"
-                            src={carSchedule?.car?.image}
-                          />
-                          <AvatarFallback>
-                            {<RiPoliceCarLine size={36} />}
-                          </AvatarFallback>
-                        </Avatar>
-                        {/* <div> */}
-                        {/*  <p className="text-sm font-medium leading-none"> */}
-                        {/*    {carSchedule?.car?.prefix}{' '} */}
-                        {/*  </p> */}
-                        {/* </div> */}
-                        {/* <Select defaultValue="edit"> */}
-                        {/*  <SelectTrigger className="ml-auto w-[110px]"> */}
-                        {/*    <SelectValue placeholder="Selecione" /> */}
-                        {/*  </SelectTrigger> */}
-                        {/*  <SelectContent> */}
-                        {/*    <SelectItem value="edit"> */}
-                        {/*      {carSchedule?.car?.plate} */}
-                        {/*    </SelectItem> */}
-                        {/*  </SelectContent> */}
-                        {/* </Select> */}
-
-                        {/* {carSchedule?.members?.map( */}
-                        {/*  (carMembers, indexMember) => ( */}
-                        {/*    <div */}
-                        {/*      key={indexMember} */}
-                        {/*      className="flex items-center justify-center gap-1" */}
-                        {/*    > */}
-                        {/*      {carMembers?.name} */}
-                        {/*      <Avatar */}
-                        {/*        className="flex h-8 w-8 items-center justify-center  rounded-full shadow-sm shadow-foreground transition-all */}
-                        {/*          duration-300 hover:scale-[200%] md:h-10 md:w-10" */}
-                        {/*      > */}
-                        {/*        <AvatarImage */}
-                        {/*          className="aspect-square rounded-full object-cover" */}
-                        {/*          src={ */}
-                        {/*            carMembers?.image ?? */}
-                        {/*            process.env.NEXT_PUBLIC_API_GSO + */}
-                        {/*              '/public/images/img.png' */}
-                        {/*          } */}
-                        {/*        /> */}
-                        {/*        <AvatarFallback> */}
-                        {/*          {<LuUser size={36} />} */}
-                        {/*        </AvatarFallback> */}
-                        {/*      </Avatar> */}
-                        {/*      <div> */}
-                        {/*        <p className="text-sm font-medium leading-none"> */}
-                        {/*          {carMembers?.name} */}
-                        {/*        </p> */}
-                        {/*        <p className="text-sm text-muted-foreground"> */}
-                        {/*          {carMembers?.email} */}
-                        {/*        </p> */}
-                        {/*      </div> */}
-                        {/*      <Select defaultValue="edit"> */}
-                        {/*        <SelectTrigger className="ml-auto w-[110px]"> */}
-                        {/*          <SelectValue placeholder="Selecione" /> */}
-                        {/*        </SelectTrigger> */}
-                        {/*        <SelectContent> */}
-                        {/*          <SelectItem value="edit"> */}
-                        {/*            {carMembers?.id_function} */}
-                        {/*          </SelectItem> */}
-                        {/*        </SelectContent> */}
-                        {/*      </Select> */}
-                        {/*    </div> */}
-                        {/*  ), */}
-                        {/* )} */}
+                  <div className="grid grid-cols-12">
+                    {schedule?.cars?.map((car, indexCar) => (
+                      <div key={indexCar} className="col-span-3">
+                        {car?.car?.prefix}
+                        {car.members.map((member, indexMember) => (
+                          <div key={indexMember}>{member?.name}</div>
+                        ))}
                       </div>
                     ))}
                   </div>
-                )}
-              </div>
-            ))}
-          </div>
+                </div>
+              )}
+            </div>
+          ))}
         </CardContent>
       </Card>
     </>
